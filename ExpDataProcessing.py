2dec5f6e376ef8cd9edb5e280f36ce6abfcc2f54
Abdelmalek Taoutioui <malek.taoutioui@gmail.com>
2025-03-13 12:35:42 +0100

    updated ExpDataProcessing.py

--- ExpDataProcessing.py
+++ ExpDataProcessing.py
@@ -4,6 +4,7 @@ from numpy import pi, sin, cos, sqrt
 import numpy as  np
 from utils import weight_function, Degree, MirrorBound
 
+
 # Reading the input parameters and the job ID from the external file
 file_name = "ExperimentINPUT.txt"
 with open(file_name, "r") as file:
